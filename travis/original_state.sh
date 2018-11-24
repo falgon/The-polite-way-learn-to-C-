9957bd2a96ce2df2b204e8169e6fc75fa0891420
falgon <falgon53@gmail.com>
2018-11-25 05:32:42 +0900

    add auto deploy feature

--- travis/original_state.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-bash -n $TRAVIS_BUILD_DIR/tools/plantuml/gen.sh
-bash -n $TRAVIS_BUILD_DIR/tools/doxygen/extract.sh
-bash -n $TRAVIS_BUILD_DIR/tools/swap_mr.sh
-cd $TRAVIS_BUILD_DIR
-
-make setup_env
-make test
