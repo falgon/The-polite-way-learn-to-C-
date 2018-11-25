908fd753d6106f914b160399220b0775bbb0ea09
falgon <falgon53@gmail.com>
2018-11-25 16:47:34 +0900

    fix travis 3

--- docs/search/all_6.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['primes',['primes',['../namespace_t_p_l_c_x_x17_1_1chap16__7__1_1_1v1.html#ab74a1a6faa1988316aed5d2d68e49eb5',1,'TPLCXX17::chap16_7_1::v1::primes()'],['../namespace_t_p_l_c_x_x17_1_1chap16__7__1_1_1v2.html#a17e41e8dce5ea14323c16293d0d7c6d5',1,'TPLCXX17::chap16_7_1::v2::primes()']]]
-];
--- docs/search/classes_0.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['search_5finsert',['search_insert',['../struct_t_p_l_c_x_x17_1_1chap16__7__1_1_1v2_1_1search__insert.html',1,'TPLCXX17::chap16_7_1::v2::search_insert'],['../struct_t_p_l_c_x_x17_1_1chap16__7__1_1_1v1_1_1search__insert.html',1,'TPLCXX17::chap16_7_1::v1::search_insert']]],
-  ['simply_5ffixed_5fpoint',['simply_fixed_point',['../class_t_p_l_c_x_x17_1_1chap16__8__1_1_1v1_1_1simply__fixed__point.html',1,'TPLCXX17::chap16_8_1::v1']]]
-];
